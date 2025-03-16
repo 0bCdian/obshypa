767fc3db75476e974c632fd74adeae11c17421ae
Diego Parra <diegomiguelp1996@gmail.com>
2025-03-17 00:02:07 +0100

    refactor: Api data ingestor to work with firestore on production
    - Add flags to control wether we are on prod or dev.
    - On dev script works as previously, reading from a csv, and writing to
    a json file.
    - On prod we read from a firestore database and write to it as well.

--- cmd/api_data_ingestor/main.go
+++ cmd/api_data_ingestor/main.go
@@ -1,52 +1,100 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
 
+	"cloud.google.com/go/firestore"
 	"github.com/shypa/cards-website/internal/apiclient"
 	"github.com/shypa/cards-website/internal/csvreader"
+	"github.com/shypa/cards-website/internal/database"
 	"github.com/shypa/cards-website/internal/savetojson"
 )
 
 func main() {
-	fileLocation, err := ParseFileArg()
-	var enoent *EnoentEror
-	var missingArg *MissingArgError
+	// Parse command line arguments
+	args, err := ParseArgs()
 	if err != nil {
-		if errors.As(err, &enoent) {
-			printErrorAndExit(enoent)
-		} else if errors.As(err, &missingArg) {
-			fmt.Println(missingArg)
-			Usage()
-			os.Exit(1)
-		}
+		handleParseError(err)
+		return
 	}
 
-	csvData, err := csvreader.CsvReader(fileLocation.manaboxCsv)
-	checkError(err)
+	var initialData csvreader.CsvData
+	var ctx context.Context
+	var client *firestore.Client
+
+	if args.isProd {
+		fmt.Println("Running in production mode with Firestore")
+
+		// Setup Firestore connection
+		project, exists := os.LookupEnv("PROJECT")
+		if !exists {
+			checkError(fmt.Errorf("PROJECT environment variable not found"))
+		}
+
+		ctx = context.Background()
+		client, err = database.NewCloudFirestoreClient(ctx, project)
+		checkError(err)
+		defer client.Close()
+
+		// Get data from Firestore
+		initialData, err = database.GetCardStock(ctx, client)
+		checkError(err)
+	} else {
+		// Get data from CSV file
+		initialData, err = csvreader.CsvReader(args.manaboxCsv)
+		checkError(err)
+	}
+
+	// Process data - common to both modes
+	// Get Scryfall data
 	fmt.Println("Getting scryfallData...")
-	scryfallData, err := apiclient.GetScryfallCardData(csvData)
+	scryfallData, err := apiclient.GetScryfallCardData(initialData)
 	checkError(err)
+
+	// Get card market data
 	fmt.Println("Getting cardMarketData...")
 	cardMarketData, err := apiclient.GetCardmarketData(&scryfallData)
 	checkError(err)
-	const json_filename = "api_data.json"
-	err = savetojson.SaveToSpecificJson(cardMarketData, json_filename)
-	checkError(err)
 
-	// TODO: Maybe transform cardMarketData (type []Card) into []MinimalNecessaryData to avoid having to read a .json with info we already have...
+	// Output data
+	if args.isProd {
+		// Write to Firestore
+		err = database.UpsertCards(ctx, client, &cardMarketData)
+		checkError(err)
+	} else {
+		// Write to JSON file
+		const jsonFilename = "api_data.json"
+		err = savetojson.SaveToSpecificJson(cardMarketData, jsonFilename)
+		checkError(err)
 
-	minimizeCardPriceData(json_filename)
+		// Create minimal version
+		minimizeCardPriceData(jsonFilename)
+	}
 
 	fmt.Println("Finished with the script!")
 }
 
+func handleParseError(err error) {
+	var enoent *EnoentError
+	var missingArg *MissingArgError
+
+	if errors.As(err, &enoent) {
+		printErrorAndExit(enoent)
+	} else if errors.As(err, &missingArg) {
+		fmt.Println(missingArg)
+		Usage()
+		os.Exit(1)
+	} else {
+		printErrorAndExit(err)
+	}
+}
+
 func minimizeCardPriceData(original_data_json_file string) {
 	fmt.Println("Creating minimal card file with prices...")
-
 	// Deserialize apiData.json into a MinimalNecessaryData
 	fileContent, err := os.ReadFile(original_data_json_file)
 	checkError(err)
@@ -62,7 +110,6 @@ func minimizeCardPriceData(original_data_json_file string) {
 	const json_filename = "precio_de_cartas.json"
 	err = savetojson.SaveToSpecificJson(minimalData, json_filename)
 	checkError(err)
-
 	fmt.Printf("File created at %v\n", json_filename)
 }
 
